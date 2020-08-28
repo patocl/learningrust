f4f423d86ad3011da9505ac052232f9046678a8e
patocl <patocl@users.noreply.github.com>
2020-08-28 17:40:04 +0200

    Create fibonacci.rs

---
+++ fibonacci.rs
@@ -0,0 +1,22 @@
+// to run it https://play.rust-lang.org/?version=stable&mode=debug&edition=2018&gist=63f26b1858a2e8cdc5ea6e2e8bef57de
+
+const FIB_ZERO: u64 = 1;
+const FIB_ONE: u64 = 1;
+
+fn fib(n: u64) -> u64 {
+    if n==0 {
+        FIB_ZERO
+    } else if n == 1 {
+        FIB_ONE
+    } else {
+      fib(n-1) + fib(n-2) 
+    }
+}
+
+fn main() {
+    let count = 41;
+    println!("fibonnaci of first {} numbers", count);
+    for i in 1..41 {
+        println!("{} = {}", i, fib(i))
+    }
+}
